4cd3b42227ed515bc40d06e3246c11c31711e63d
Philippe <varinotformation88@gmail.com>
2020-10-08 15:51:26 +0200

    reserve

---
+++ main.js
@@ -0,0 +1,53 @@
+    new Vue({
+        el: '#app',
+        data: {
+         niveau : ' ',
+          eleve : ['Daniel', 'Eric', 'Carole'],
+          notation1 : 0,
+          notation2 : 0,
+          notation3 : 0,
+          mdpOk: false,
+         mdp : "totordirecteur", 
+         txt: 'moyenne générale de la classe :',
+         moyenne: 0, 
+         somme: 0,
+         },
+        methods: {
+          presentation1: function(){
+             return this.eleve[0] 
+          },
+          augmente1: function(){
+             this.notation1++;
+             this.somme++;             }, 
+          diminue1: function(){
+             this.notation1--;
+             this.somme--;},
+          presentation2: function(){
+             return this.eleve[1]
+             },
+          augmente2: function(){
+          this.notation2++;
+          this.somme++;},
+          diminue2: function(){
+             this.notation2--;
+             this.somme--; },
+          presentation3: function(){
+             return this.eleve[2]
+             },
+          augmente3: function(){
+          this.notation3++;
+          this.somme++;},
+          diminue3: function(){
+             this.notation3--;
+             this.somme--; },
+      
+      //    il faut récupérer la validation par click du
+      //    bouton valider l'affichage des saisies de notes
+      //   
+      //      mdpOk: function(){
+      //      return this.moyenne ;
+      //   }
+          
+         }     
+            })
+ 
